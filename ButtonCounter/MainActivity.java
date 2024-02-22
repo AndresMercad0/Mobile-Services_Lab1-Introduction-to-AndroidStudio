5b5ef2252ea27a0accbf40c2a75e75766d1c415d
Andrés Mercado <andres@mevel.com.mx>
2024-02-22 13:51:01 +0000

    Update ButtonCounter Code

---
+++ ButtonCounter/MainActivity.java
@@ -0,0 +1,33 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Incrementar el contador al hacer clic en el botón
+                counter++;
+                // Actualizar el texto del TextView con el nuevo valor del contador
+                textView.setText("Contador: " + counter);
+            }
+        });
+    }
+}
